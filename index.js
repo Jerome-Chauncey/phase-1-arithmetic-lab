81f3c9391d04de9326b3d3ea7abb1452aa086185
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-04 20:44:17 +0300

    Performs arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 28;
+let num4 = 6;
+const mod = num3 % num4;
+
+let max = Math.max(5,10,15,20);
